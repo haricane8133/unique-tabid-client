c422d1a52088b746b418cf14648c29bf1d66a526
Hari Rajesh <hari.r.nallan@gmail.com>
2022-10-31 15:06:04 +0530

    Using unique-tabid@1.0.2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import {useEffect, useState} from "react";
+import uniqid from "uniqid";
+import UniqueTabId from "unique-tabid";
 import logo from './logo.svg';
 import './App.css';
 
+const uniqueTabId = new UniqueTabId("testapp");
+uniqueTabId.WAIT_TIMEOUT = 2000;
+uniqueTabId.uniqIdFunc = uniqid;
+
 function App() {
+  const [text, setText] = useState("");
+  uniqueTabId.tabidCallback = (tabid, wasDuplicated, isNewTab) => setText(`TabId: ${tabid}; Duplicated: ${wasDuplicated}; New Tab: ${isNewTab}`);
+  useEffect(() => uniqueTabId.initTab(), []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{text}</p>
       </header>
     </div>
   );
